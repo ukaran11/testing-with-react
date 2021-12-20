67260c6ae2195fb7e4cd87b553a4a043b72f95db
Utsav Karan <ukaran11@gmail.com>
2021-12-21 00:34:54 +0530

    Utsav | Added setup for test file

--- src/component/headline/headline.test.js
+++ src/component/headline/headline.test.js
@@ -12,9 +12,20 @@ const setUp = (props={}) => {
 describe('Headline Component', () => {
     describe('Have props', () => {
 
+        let wrapper;
+        beforeEach(() => {
+            const props = {
+                header: 'Test Header',
+                desc: 'Test Desc'
+            };
+            wrapper = setUp(props);
+        });
     });
 
     describe('Have No Props', () => {
-        
-    })
-})
+        let wrapper;
+        beforeEach(() => {
+            wrapper = setUp();
+        });
+    });
+});
