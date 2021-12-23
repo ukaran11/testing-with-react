bd685a06bdcc430bd90bc7f962af6ccf17d262a1
Utsav Karan <ukaran11@gmail.com>
2021-12-24 02:07:35 +0530

    Utsav | Attached click handler on button

--- src/component/button/index.js
+++ src/component/button/index.js
@@ -2,10 +2,18 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types';
 
 class SharedButton extends Component {
+    
+    submitEvent() {
+        if(this.props.emitEvent) {
+            this.props.emitEvent();
+        }
+    }
     render() {
+        const { buttonText } = this.props;
+
         return (
-            <button data-test="buttonComponent">
-                
+            <button onClick={() => this.submitEvent()} data-test="buttonComponent">
+                { buttonText }
             </button>
         )
     }
