f0135e224a335441ad1c1e42d2553ced8125570f
Utsav Karan <ukaran11@gmail.com>
2021-12-24 01:27:30 +0530

    Utsav | Created boilterplate component button

---
+++ src/component/button/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types';
+
+class SharedButton extends Component {
+    render() {
+        return (
+            <button>
+                
+            </button>
+        )
+    }
+}
+
+SharedButton.propTypes = {
+
+};
+
+export default SharedButton;
