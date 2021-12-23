e5032978e94d9e78d4f6e96ed8beb81f805c32b9
Utsav Karan <ukaran11@gmail.com>
2021-12-24 00:48:09 +0530

    Utsav | Created switch stmt for post reducer

--- src/reducers/posts/reducer.js
+++ src/reducers/posts/reducer.js
@@ -1,5 +1,12 @@
 import { types } from "../../actions/types";
 
-export default (state=[], action) => {
-    return null;
+const reducer = (state=[], action) => {
+    switch(action.type){
+        case types.GET_POSTS:
+            return action.payload;
+        default:
+            return state;
+    }
 }
+
+export default reducer;
