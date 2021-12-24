ae2ca0c23602afe4e8e4e208a619b7606bc97816
Utsav Karan <ukaran11@gmail.com>
2021-12-25 01:59:31 +0530

    Utsav | Created ListemItem comp boilerplate

---
+++ src/component/listItem/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class ListItem extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+ListItem.propTypes = {
+    title: PropTypes.string,
+    desc: PropTypes.string
+}
+
+export default ListItem 
