7767ee12d41fa460a3e8877c639143b87e1509e5
Utsav Karan <ukaran11@gmail.com>
2021-12-27 00:20:45 +0530

    Utsav | Added test for basic render w/o errors

--- src/App.test.js
+++ src/App.test.js
@@ -5,7 +5,8 @@ import { findByTestAttr, testStore } from '../Utils';
 
 const setUp = (initialState) => {
     const store = testStore(initialState);
-    const wrapper = shallow(<App store={store} />);
+    const wrapper = shallow(<App store={store} />).childAt(0).dive();
+    console.log(wrapper.debug())
     return wrapper;
 };
 
@@ -23,8 +24,13 @@ describe('App Component', () => {
             },{
                 title: 'Example title 3',
                 body: 'Some text'
-            }];
+            }]
         }
         wrapper = setUp(initialState);
     });
+
+    it('Should render without errors', () => {
+        const component = findByTestAttr(wrapper, 'appComponent');
+        expect(component.length).toBe(1);
+    })
 });
